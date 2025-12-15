409c162d0d5052ef96b98d22426abaeb6c8ccf24
Matthew Jia <dwangmj27@gmail.com>
2025-12-15 15:44:20 -0500

    finished day 3

--- aoc2024/src/bin/day03.rs
+++ aoc2024/src/bin/day03.rs
@@ -1,82 +1,76 @@
 use aoc2024;
-use itertools::Either;
+use std::cmp;
 
 fn main() 
 {
     let input = aoc2024::read_input(3);
-    let l: Vec<Vec<i32>> = parse_input(&input);
 
-    println!("answer to part 1 = {}", part1(&l));
-    println!("answer to part 2 = {}", part2(&l));
+    println!("answer to part 1 = {}", part1(&input));
+    println!("answer to part 2 = {}", part2(&input));
 }
 
-fn parse_input(input: &str) -> Vec<Vec<i32>> 
+fn part1(l: &str) -> i32 
 {
-    let mut l: Vec<Vec<i32>> = Vec::new();
+    let mut res = 0;
+    let max_substr_len = 3 + 2 + 3 + 1 + 3; // 3 for mut, 2 for (), 3 for first int, 1 for
+                                                // comma, 3 for second int
 
-    for (i, line) in input.lines().enumerate() 
+    for idx in 0..l.len()-7
     {
-        l.push(Vec::new());
+        let substr_len = cmp::min(max_substr_len, l.len()-idx); // get the length of the maximum possible
+                                                                // mul substr starting at idx
 
-        for val in line.split_whitespace() 
-        {
-            l[i].push(val.parse().unwrap());
-        }
+        let mul = &l[idx..idx+substr_len];
+        
+        let nums: Vec<i32> = mul
+            .split(|c: char| !c.is_ascii_digit())
+            .filter(|t| !t.is_empty())
+            .map(|t| t.parse().unwrap())
+            .collect();
+
+        if nums.len() < 2 { continue }
+
+        let final_string = format!("mul({},{})", nums[0], nums[1]);
+        if mul[0..final_string.len()] == final_string { res += nums[0] * nums[1]; }
     }
-
-    l
+    
+    res
 }
 
-fn check_vec(vec: &[i32]) -> bool 
+fn part2(l: &str) -> i32
 {
-    let (Some(first), Some(last)) = (vec.first(), vec.last()) else { return false };
-    let pairs = match first.cmp(last) 
-    {
-        std::cmp::Ordering::Less    => Either::Left(vec.iter().zip(vec.iter().skip(1))),
-        std::cmp::Ordering::Greater => Either::Right(vec.iter().rev().zip(vec.iter().rev().skip(1))),
-        std::cmp::Ordering::Equal   => return false
-    };
+    let mut res = 0;
+    let max_substr_len = 3 + 2 + 3 + 1 + 3; // 3 for mut, 2 for (), 3 for first int, 1 for
+                                                // comma, 3 for second int
+    
+    let mut do_mul: bool = true;
+    let do_len = 4;
+    let dont_len = 7;
 
-    for (&cur, &next) in pairs
+    for idx in 0..l.len()-7
     {
-        if next < cur + 1 { return false }
-        if next > cur + 3 { return false }
+        let substr_len = cmp::min(max_substr_len, l.len()-idx); // get the length of the maximum possible
+                                                                // mul substr starting at idx
+
+        let s = &l[idx..idx+substr_len];
+
+        if s[0..do_len] == *"do()"       { do_mul = true; continue; }
+        if s[0..dont_len] == *"don't()"  { do_mul = false; continue; }
+
+        if !do_mul { continue; }
+        
+        let nums: Vec<i32> = s
+            .split(|c: char| !c.is_ascii_digit())
+            .filter(|t| !t.is_empty())
+            .map(|t| t.parse().unwrap())
+            .collect();
+
+        if nums.len() < 2 { continue }
+
+        let final_string = format!("mul({},{})", nums[0], nums[1]);
+        if s[0..final_string.len()] == final_string { res += nums[0] * nums[1]; }
     }
-
-    true
-}
-
-fn part1(l: &[Vec<i32>]) -> i32 
-{
-    let mut count = 0;
-
-    l.iter().for_each(|vec| {
-        if check_vec(vec) { count += 1; }
-    });
-
-    count
-}
-
-fn part2(l: &[Vec<i32>]) -> i32
-{
-    let mut count = 0;
-
-    'outer: for vec in l
-    {   
-        for i in 0..vec.len()
-        {
-            let mut vec_clone = vec.clone();
-            vec_clone.remove(i);
-
-            if check_vec(&vec_clone) 
-            {
-                count += 1;
-                continue 'outer;
-            }
-        }
-    };
-
-    count
-
+    
+    res
 }
 
